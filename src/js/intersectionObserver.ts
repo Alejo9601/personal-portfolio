b6774f370d4e4c036139f3cd5fb0a2c8311841ee
Alejo9601 <alejandro96jm@gmail.com>
2024-03-13 16:34:50 -0300

    Added instersection observer script

---
+++ src/js/intersectionObserver.ts
@@ -0,0 +1,27 @@
+const makeElementVisible = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {  
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.remove("paused")
+            console.log(entry)
+        }
+    })
+}
+
+window.addEventListener("DOMContentLoaded", ()=> {
+    const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.50,
+    }
+
+    
+    let observer = new IntersectionObserver(makeElementVisible, options);
+    
+    let observables = document.querySelectorAll(".section")
+
+    observables.forEach((el) => {
+        observer.observe(el)
+    })  
+
+})
+
